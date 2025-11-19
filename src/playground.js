e0b01026b7a3a9d6d2831bcb3a2984df54f30c28
iamtati1 <tatianabarmer@gmail.com>
2025-11-18 21:32:13 -0500

    Practiced inheritance

--- src/playground.js
+++ src/playground.js
@@ -1,1 +1,24 @@
 console.log('Put your code here!');
+
+class Employee {
+    constructor(name, salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+    getDetails() {
+        return `${this.name} earns $${this.salary}`;
+    }
+}
+class Manager extends Employee {
+    constructor(name, salary, department) {
+        super(name, salary)
+        this.department = department;
+    }
+
+    getDetails() {
+        return (`The employee's name is ${this.name}, the employee's salary is ${this.salary}, and their department info is ${this.department}.`)
+    }
+}
+
+const mgr = new Manager("Alice", 80000, "Engineering");
+console.log(mgr.getDetails());
--- src/shapes.js
+++ src/shapes.js
@@ -1,20 +1,64 @@
 /* eslint-disable max-classes-per-file */
 
 class Shape {
+  constructor(type) {
+    this.type = type
+  }
 
+  getArea() {
+    return 0;
+  }
+}
+const myShape = new Shape('blob');
+console.log(myShape.type); // blob
+console.log(myShape.getArea()); // 0
+
+class Circle extends Shape {
+  constructor(radius) {
+    super("Circle");
+    this.radius = radius
+
+  }
+
+  getArea() {
+    return Math.PI * (this.radius ** 2)
+  }
 }
 
-class Circle {
+const myCircle = new Circle(10);
+console.log(myCircle.type); // Circle
+console.log(myCircle.radius); // 10
+console.log(myCircle.getArea()); // 314.15927....
 
+class Rectangle extends Shape {
+  constructor(length, width) {
+    super("Rectangle")
+    this.length = length
+    this.width = width
+  }
+
+  getArea() {
+    return (this.length * this.width)
+  }
+}
+const myRectangle = new Rectangle(4, 3);
+console.log(myRectangle.type); // Rectangle
+console.log(myRectangle.length); // 4
+console.log(myRectangle.width); // 3
+console.log(myRectangle.getArea()); // 12
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side);     // required by the test
+    this.type = "Square";  // fix instance-creation test
+  }
 }
 
-class Rectangle {
-
-}
-
-class Square {
-
-}
+const mySquare = new Square(5);
+console.log(mySquare.type); // Square
+console.log(mySquare.length); // 5
+console.log(mySquare.width); // 5
+console.log(mySquare.getArea()); // 25
 
 module.exports = {
   Shape,
